b4687f8120b12c154b03629d2bc364a95e2f40d0
Minyvvvv <Minyvvvv@outlook.com>
2026-04-21 17:07:44 +0800

    2026/4/21提交内容： 1。需要提出的是第一次的提交时间写错了，应为20. 2.本次提交新加入了学生成绩管理系统和学生类，成绩管理包含增添学生，删除学生，修改成绩，查询成绩。作为初期项目仍有很大的改进空间，需要继续学习来对代码进行改进，具体修改方向见注释。 3.大学课程阿米诺斯。

---
+++ src/ScoreManagement.java
@@ -0,0 +1,12 @@
+public class score_management {
+    public class Student{
+        private String id;
+        private String name;
+        private double score;
+    }
+
+    public score_management(String id , ) {
+    }
+
+
+}
